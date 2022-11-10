c8ee51c5532a1d8d4f478b119cf76bbe6ec9b8d0
aasimsyed97 <sayedaasim555@gmail.com>
2022-11-10 22:22:35 +0530

    second push

---
+++ Syed_Auction/src/Exceptions/BuyerException.java
@@ -0,0 +1,22 @@
+package Exceptions;
+
+public class BuyerException extends Exception{
+
+	public BuyerException() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	
+
+	public BuyerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	 
+
+}
