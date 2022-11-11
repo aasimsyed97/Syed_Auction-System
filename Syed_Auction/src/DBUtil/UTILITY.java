6cb38ba0e1d542658e0f9e70107b69e8dffc6ecb
aasimsyed97 <sayedaasim555@gmail.com>
2022-11-11 21:51:25 +0530

    push day4

--- Syed_Auction/src/DBUtil/UTILITY.java
+++ Syed_Auction/src/DBUtil/UTILITY.java
@@ -1,5 +1,45 @@
 package DBUtil;
 
-public class UTILITY {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-}
+
+public class UTILITY {
+	
+
+	
+		
+		
+		public static Connection provideConnection() {
+			
+			Connection conn = null;
+			
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			String url= "jdbc:mysql://localhost:3306/web20";
+			
+			try {
+				conn = DriverManager.getConnection(url, "root", "13CE053@aasim");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+			return conn;
+		}
+
+	}
+
+
+
