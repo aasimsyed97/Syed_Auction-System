d057eedf3bf45e51f29587c21707e5789e4f3455
aasimsyed97 <sayedaasim555@gmail.com>
2022-11-12 14:29:37 +0530

    day5 first

---
+++ Syed_Auction/src/Dao/SellerDao.java
@@ -0,0 +1,20 @@
+package Dao;
+
+import Exceptions.SellerException;
+
+public interface SellerDao {  
+	
+	
+//	. Register himself or herself as seller.
+//	B. Create list of items to sell.
+//	C. Update Item price, quantity, etc.
+//	D. Add new Item in the list.
+//	E. Remove items from the list.
+//	F. View the sold Item history. 
+	
+	//public String registSeller(Seller seller)throws SellerException; 
+	public String  itemsForSell()
+
+	 
+
+}
