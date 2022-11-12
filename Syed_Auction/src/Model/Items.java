0f8e99a9c9ec62ff1772b97c2ee3556984190252
aasimsyed97 <sayedaasim555@gmail.com>
2022-11-13 00:30:31 +0530

    itemslist

---
+++ Syed_Auction/src/Model/Items.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Items { 
+	
+	
+	
+  private  int Itemid ;
+  private String name;
+  private String category;
+  private int quantity; 
+  private int price ;
+  private String status;
+  private int Sellerid;
+  
+  
+  
+}
